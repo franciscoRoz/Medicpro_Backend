84b767a71091b3e9a19fc75094c54901da3eb6b7
Francisco Rozas <francisco.rozas@outlook.com>
2024-04-15 08:28:06 -0400

    push version

---
+++ Controller/Facturas/POST/CrearDespacho.js
@@ -0,0 +1,24 @@
+const { response } = require("express");
+const { InsertarItem } = require("../../../Component/MongoDB/InsertarItem");
+const { Now } = require("../../../Utility/LocalTime");
+
+const CrearDespacho = async (req, res = response) => {
+  try {
+    let Despacho = req.body;
+    Despacho.createAt = Now();
+    InsertarItem(Despacho, "Despacho");
+    res.send({ succes: true, ok: "OK" }).status(200);
+  } catch (e) {
+    console.log(e);
+    res
+      .send({
+        succes: false,
+        estado: "No se pudo agregar el producto",
+      })
+      .status(400);
+  }
+};
+
+module.exports = {
+    CrearDespacho,
+};
--- Routes/DespachosyFacturas.js
+++ Routes/DespachosyFacturas.js
@@ -2,10 +2,12 @@
 const { Router } = require("express");
 const { CrearFactura } = require("../Controller/Facturas/POST/CreateFactura");
 const { ObtenerFacturas } = require("../Controller/Facturas/GET/GetFacturas");
+const { CrearDespacho } = require("../Controller/Facturas/POST/CrearDespacho");
 
 
 const router = Router();
 
 router.post("/crearfactura", CrearFactura);
 router.get("/obtenerfacturas",ObtenerFacturas)
+router.post("/creardespacho",CrearDespacho)
 module.exports = router;
