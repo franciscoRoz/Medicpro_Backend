9727e32b612cbfe0df4e0c5ee298f73eea1e5fcc
Francisco Rozas <francisco.rozas@outlook.com>
2024-08-14 21:23:23 -0400

    correcciones producto

--- Routes/GADM.js
+++ Routes/GADM.js
@@ -23,6 +23,7 @@ const { GetproductosCompletoProveedor } = require("../Controller/GestAdm-Stock/G
 const { insertarStock } = require("../Controller/GestAdm-Stock/POST/InsertarStock");
 const { CrearProductomasivo } = require("../Controller/GestAdm-Stock/POST/CrearProductoMasivo");
 const { ActualizarProducto_Item } = require("../Controller/GestAdm-Stock/POST/ActualizarStockProducto");
+const { Get_Product } = require("../Controller/GestAdm-Stock/GET/Get_product");
 //const { Get_Product } = require("../Controller/GestAdm-Stock/GET/Get_product");
 
 
@@ -36,7 +37,7 @@ router.get("/producto", Get_productos);
 router.post("/actualizarproducto",ActualizarProducto)
 router.post("/eliminarproducto",EliminarProducto)
 router.get("/obtenerstock/:id",Get_Stock)
-//router.get("/obtenerproducto/:id",Get_Product)
+router.get("/obtenerproducto/:id",Get_Product)
 router.post("/insertarStock",insertarStock)
 router.post("/crearproductomasivo", CrearProductomasivo);
 router.post("/actualizaritem",ActualizarProducto_Item)
